6933921bd9a9f991ea4deb39b2bcc06b38e16ae2
Draugr587 <lukashllc@gmail.com>
2026-04-27 23:14:47 -0400

    api modificacion

---
+++ modificacion/src/main/java/com/multigacha/modificacion/ModificacionApplication.java
@@ -0,0 +1,13 @@
+package com.multigacha.modificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModificacionApplication.class, args);
+	}
+
+}
---
+++ modificacion/src/test/java/com/multigacha/modificacion/ModificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multigacha.modificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
