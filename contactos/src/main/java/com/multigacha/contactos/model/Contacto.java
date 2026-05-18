7daa548d47fbb8af5340ec67708981686cf4c7d4
Draugr587 <lukashllc@gmail.com>
2026-05-17 20:12:23 -0400

    modified:   contactos/src/main/java/com/multigacha/contactos/model/Contacto.java

--- contactos/src/main/java/com/multigacha/contactos/model/Contacto.java
+++ contactos/src/main/java/com/multigacha/contactos/model/Contacto.java
@@ -1,5 +1,14 @@
 package com.multigacha.contactos.model;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class Contacto {
+
+    private Integer id;
+    private Integer telefono;
+    private String direccion;
 
 }
