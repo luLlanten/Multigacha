094854d12f3238a8af1cc68bb814d938bd11baa7
Draugr587 <lukashllc@gmail.com>
2026-05-17 21:12:35 -0400

    modified:   contactos/src/main/java/com/multigacha/contactos/model/Contacto.java

--- contactos/src/main/java/com/multigacha/contactos/model/Contacto.java
+++ contactos/src/main/java/com/multigacha/contactos/model/Contacto.java
@@ -1,14 +1,29 @@
 package com.multigacha.contactos.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table
+@Table(name = "contacto")
 public class Contacto {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false, length = 8)
     private Integer telefono;
+
+    @Column
     private String direccion;
 
 }
