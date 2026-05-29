d24cf1428738afbba13247a4f781c3a73ddcf226
Draugr587 <lukashllc@gmail.com>
2026-05-29 18:16:09 -0400

    modified:   compra/src/main/java/com/multigacha/compra/client/CatalogoClient.java

--- compra/src/main/java/com/multigacha/compra/client/CatalogoClient.java
+++ compra/src/main/java/com/multigacha/compra/client/CatalogoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalogo-service", url = "http://localhost:8081/api/v1/catalogo")
+@FeignClient(name = "catalogo", url = "http://localhost:8081/api/v1/catalogo")
 public interface CatalogoClient {
     
     @PutMapping("/{id}/reducir-stock/{cantidad}")
